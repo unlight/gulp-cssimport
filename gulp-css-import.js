f9ab7184dac788568a54053ce524fff595398602
iamthes <s@home.local>
2014-01-24 02:04:52 +0400

    Playing

--- gulp-css-import.js
+++ gulp-css-import.js
@@ -1,50 +1,34 @@
-// through2 is a thin wrapper around node transform streams
 var through = require('through2');
 var gutil = require('gulp-util');
+var es = require('event-stream');
+var fs = require("fs");
+var Stream = require("stream");
 var PluginError = gutil.PluginError;
 
-// Consts
 const PLUGIN_NAME = 'gulp-css-import';
 
-function prefixStream(prefixText) {
-	var stream = through();
-	stream.write(prefixText);
-	return stream;
-}
+module.exports = function(options) {
+	'use strict';
+	
+	options = options || {};
 
-// Plugin level function (dealing with files)
-function gulpPrefixer(prefixText) {
+	function mapFunction(file, callback) {
 
-	//console.log(require("util").inspect(prefixText));
+		var lines = String(file.contents).split("\n");
+		console.log(lines);
 
-	if (!prefixText) {
-		throw PluginError(PLUGIN_NAME, "Missing prefix text!");
+		// // Split Strings
+		// file.pipe(es.split("\n"))
+		//   .pipe(es.mapSync(function(data) {
+  //       	console.log(data);
+  //   		}))
+		// // Replace
+		// // .pipe(es.replace("John", "Bob"))
+		// // Join Strings
+		// .pipe(es.join("\n"));
+
+		callback(null, file);
 	}
-	prefixText = new Buffer(prefixText); // allocate ahead of time
 
-	// Creating a stream through which each file will pass
-	var stream = through.obj(function (file, enc, callback) {
-		if (file.isNull()) {
-			this.push(file); // Do nothing if no contents
-			return callback();
-		}
-
-		if (file.isBuffer()) {
-			file.contents = Buffer.concat([prefixText, file.contents]);
-			this.push(file);
-			return callback();
-		}
-
-		if (file.isStream()) {
-			file.contents = file.contents.pipe(prefixStream(prefixText));
-			this.push(file);
-			return callback();
-		}
-	});
-
-	// returning the file stream
-	return stream;
-};
-
-// Exporting the plugin main function
-module.exports = gulpPrefixer;
+	return es.map(mapFunction);
+}
